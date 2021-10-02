ebdba172d54cc7e46c026cc204edfbd9694eb960
srusti610 <79088553+srusti610@users.noreply.github.com>
2021-10-02 13:34:25 +0530

    Create Error_corrected.cc

---
+++ Error_corrected.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[20],country[20],skill;
+    int age;
+    printf("Welcome to Hacktoberfest 2020\n");
+    printf("Enter your name : ");//Get User name
+    scanf("%s",&name);
+    printf("Enter your Country of Residance :  ");//Get user country
+    scanf("%s",&country);
+    printf("Enter your Age : ");//Get user Age
+    scanf("%d",&age);
+    printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
+    scanf(" %c",&skill);
+    printf("Thank you %s Happy Hacking",name);//Thank the user
+
+    return 0;
+}
